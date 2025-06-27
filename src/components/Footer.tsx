1c91643c295af1a0e700c00cdf010a95ae76e94a
Javier Prz <javierpsantos99@gmail.com>
2025-06-27 12:39:51 +0200

    footer complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,19 +7,19 @@ export default function Footer() {
 
         {/* Redes sociales */}
         <div className="flex space-x-5 mt-4 md:mt-0 text-xl">
-          <a href="https://github.com/tuusuario" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
+          <a href="https://github.com/jvprz" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
             <i className="fab fa-github hover:text-red"></i>
           </a>
-          <a href="https://linkedin.com/in/tuusuario" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
+          <a href="https://www.linkedin.com/in/jvprz/" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
             <i className="fab fa-linkedin hover:text-red"></i>
           </a>
-          <a href="https://x.com/tuusuario" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
+          <a href="https://x.com/jvprz__" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
             <i className="fab fa-x-twitter hover:text-red"></i>
           </a>
-          <a href="https://instagram.com/tuusuario" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
+          <a href="https://www.instagram.com/jvprz_" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
             <i className="fab fa-instagram hover:text-red"></i>
           </a>
-          <a href="https://discord.gg/tuinvitacion" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
+          <a href="https://discord.com/users/351742989518766090" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
             <i className="fab fa-discord hover:text-red"></i>
           </a>
         </div>
