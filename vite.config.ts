5361eb4664032b78ab24b4600e94047bcf459d24
Javier Prz <javierpsantos99@gmail.com>
2025-07-23 16:17:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/",  // raíz, porque repo user-page
-  build: {
-    outDir: "docs", // para que GitHub Pages sirva desde docs/
-    assetsDir: "assets",
-  },
   plugins: [react()],
-});
+  build: {
+    outDir: 'docs',
+  },
+})
